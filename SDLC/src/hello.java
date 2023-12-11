9fb6908ff6fff59ca691073abe11309f1eaf3b27
kovairadmin <kovairadmin@WIN-VRGA3V1UMDK>
2023-12-10 23:28:56 -0800

    19

--- SDLC/src/hello.java
+++ SDLC/src/hello.java
@@ -2,6 +2,6 @@
 public class hello {
 	public static void main(String args[])
 	{
-		System.out.println("Hello solar system I am there....");
+		System.out.println("Hello solar system I am....");
 	}
 }
