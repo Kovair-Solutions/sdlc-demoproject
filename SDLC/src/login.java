65e6b65df25e60d63487f1904b30bda4f6d01fcf
kovairadmin <kovairadmin@WIN-VRGA3V1UMDK>
2023-12-14 20:30:27 -0800

    34

--- SDLC/src/login.java
+++ SDLC/src/login.java
@@ -2,6 +2,11 @@
 public class login {
 	public static void main(String args[])
 	{
-		System.out.println("Loggin On....");
+		System.out.println("================================================================");
+		System.out.println("\tWELCOME TO COLOIDAL CALCULATOR\n");
+		System.out.println("\tENTER YOUR USERNAME:- ");
+		System.out.println("\tENTER YOUR PASSWORD:- ");
+		System.out.println("================================================================");
+		System.out.println("================================================================");
 	}
 }
