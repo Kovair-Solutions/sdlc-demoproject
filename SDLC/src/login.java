754ab9e4e0e63c3abc9534d537012188406cc2a0
kovairadmin <kovairadmin@WIN-VRGA3V1UMDK>
2023-12-19 21:43:36 -0800

    39

--- SDLC/src/login.java
+++ SDLC/src/login.java
@@ -6,6 +6,7 @@ public class login {
 		System.out.println("\tWELCOME TO COLOIDAL CALCULATOR\n");
 		System.out.println("\tENTER YOUR USERNAME:- ");
 		System.out.println("\tENTER YOUR PASSWORD:- ");
+		System.out.println("\tNew Commit added");
 		System.out.println("================================================================");
 		System.out.println("================================================================");
 	}
